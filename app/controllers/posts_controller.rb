0cd698fe51e0e01eb4a74fdf511e4e72a894a080
Zakrevsky A.A <Asistent01@gmail.com>
2021-02-03 22:22:47 +0300

    Edit controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,25 @@
 class PostsController < ApplicationController
 
-  def new
-    @post = Post.new
-  end
+def new
+@post = Post.new
+end
 
-  def create
-      @post = Post.new(post_params)
-      if @post.save
-        redirect_to profile_path, flash[:notice] = "Post successfully created"
-      else
-        redirect_to new_post_path, flash: {alert: "Some errors"}
+def create
+@post = Post.new(post_params)
+if @post.save
+redirect_to dash_path, flash: {success: "Post was created"}
+else
+render :new, flash: {alert: "Some errors"}
 
-      end
-  end
+end
+end
 
-  def show
-    @posts = Post.all
-  end
+def show
+end
 
-  private
+private
 
-  def post_params
-    params.require(:post).permit(:image, :image_cache, :description)
-  end
+def post_params
+params.require(:post).permit(:image, :image_cache)
+end
 end
