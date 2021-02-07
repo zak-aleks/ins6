7aa3cf8ff98e59768eb52223b3c01254bf64f3aa
Zakrevsky A.A <Asistent01@gmail.com>
2021-02-08 01:07:10 +0300

    Edit styles of pic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
-     @posts = Post.all
+  
+     @posts = Post.all.order(created_at: :desc)
   end
 end
